745d19e51066e9d587fa379ca2ff870a5417d3e7
AJ Aparicio <aparicio.anthonyjohn@gmail.com>
2025-11-25 18:22:34 +0800

    style: shrink icon design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,12 @@
-import React from "react";
-
+"use client";
+import React, { useEffect } from "react";
+import { useRouter } from "next/navigation";
 const App = () => {
+  const router = useRouter();
+  // Redirect to /login
+  useEffect(() => {
+    router.push("/login");
+  }, []);
   return <div>App</div>;
 };
 
